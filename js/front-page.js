725c5c980fb48b577d8bacf90a277cddad0221ef
Scott Nix <scott@scottnix.com>
2016-07-25 18:53:28 -0700

    First Push
    Theme is ready to be overhauled into a faster workflow for myself.
    Pushing it to github to track changes since they will be somewhat major.

---
+++ js/front-page.js
@@ -0,0 +1,25 @@
+jQuery(function( $ ){
+
+	// Set front page 1 height
+	// scott - trimming height so it isn't full sized
+	//  var windowHeight = $( window ).height() - 90;
+	var windowHeight = $( window ).height() - 350;
+
+	$( '.front-page-1' ) .css({'height': windowHeight +'px'});
+		
+/*	$( window ).resize(function(){
+	
+		// scott - stop it from reseting to full page on resize.... lame
+		// var windowHeight = $( window ).height();
+		var windowHeight = $( window ).height() - 150;
+	
+		$( '.front-page-1' ) .css({'height': windowHeight +'px'});
+	
+	});*/
+
+	// Local Scroll Speed
+	$.localScroll({
+		duration: 750
+	});
+
+});
